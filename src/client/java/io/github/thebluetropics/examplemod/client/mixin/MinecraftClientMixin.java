7ab3a7c7619082961b4a6a4f28d088be677b2129
thebluetropics <thebluetropics@gmail.com>
2024-12-03 18:58:40 +0700

    rename mixin classes

--- src/client/java/io/github/thebluetropics/examplemod/client/mixin/MinecraftClientMixin.java
+++ src/client/java/io/github/thebluetropics/examplemod/client/mixin/MinecraftClientMixin.java
@@ -8,7 +8,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MinecraftClient.class)
-public class TestClientMixin {
+public class MinecraftClientMixin {
   @Inject(at = @At("HEAD"), method = "run")
   private void run(CallbackInfo info) {
     ExampleModClient.LOGGER.info("Hello, World! (Client mixin)");

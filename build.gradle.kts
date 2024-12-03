8a6e8b44d0e2fe5c53a08345e0723dbe7ec4cc1d
thebluetropics <thebluetropics@gmail.com>
2024-12-03 14:06:45 +0700

    refactor artifact naming and versioning

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,15 +3,34 @@ plugins {
   id("maven-publish")
 }
 
+// Maven
+val artifactName        = properties["artifact_name"]         as String
+
+// Mod
+val modId               = properties["mod_id"]                as String
+
+// Minecraft
+val minecraftVersion    = properties["minecraft_version"]     as String
+
+// Fabric
+val fabricLoaderVersion = properties["fabric_loader_version"] as String
+val fabricApiVersion    = properties["fabric_api_version"]    as String
+
+// Mappings
+val mappingsVersion     = properties["mappings_version"]      as String
+
+// Github
+val githubPackagesUrl   = properties["github_packages_url"]   as String
+
 base {
-  archivesName = "${properties["artifact_name"].toString()}-fabric"
+  archivesName = "${artifactName}-fabric-${minecraftVersion}"
 }
 
 dependencies {
-  minecraft("com.mojang:minecraft:${properties["minecraft_version"].toString()}")
-  mappings("net.fabricmc:yarn:${properties["mappings_version"].toString()}:v2")
-  modImplementation("net.fabricmc:fabric-loader:${properties["fabric_loader_version"].toString()}")
-  modImplementation("net.fabricmc.fabric-api:fabric-api:${properties["fabric_api_version"].toString()}")
+  minecraft("com.mojang:minecraft:${minecraftVersion}")
+  mappings("net.fabricmc:yarn:${mappingsVersion}:v2")
+  modImplementation("net.fabricmc:fabric-loader:${fabricLoaderVersion}")
+  modImplementation("net.fabricmc.fabric-api:fabric-api:${fabricApiVersion}")
 }
 
 loom {
@@ -27,7 +46,7 @@ loom {
   }
 
   mods {
-    create(properties["mod_id"].toString()) {
+    create(modId) {
       sourceSet("main")
     }
   }
@@ -60,7 +79,7 @@ tasks.processResources {
 tasks.named<Jar>("jar") {
   from("LICENSE") {
     rename {
-      "LICENSE-${properties["artifact_name"]}"
+      "LICENSE-${artifactName}"
     }
   }
 }
@@ -83,7 +102,7 @@ publishing {
     }
     maven {
       name = "GithubPackages"
-      url = uri(properties["github_packages_url"].toString())
+      url = uri(githubPackagesUrl)
       credentials {
         username = System.getenv("GITHUB_ACTOR")
         password = System.getenv("GITHUB_TOKEN")

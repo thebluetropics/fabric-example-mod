433b58c86dd45a6519fb9f1f6d8b2a67c1f92345
thebluetropics <thebluetropics@gmail.com>
2025-03-20 07:17:05 +0700

    refactor build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,22 +3,32 @@ plugins {
 	id("maven-publish")
 }
 
+val modId = properties["mod_id"] as String
 val minecraftVersion = properties["minecraft_version"] as String
 
 base {
-	archivesName = "${properties["artifact_name"].toString()}-fabric-${minecraftVersion}"
+	archivesName = "${properties["artifact_name"]}-fabric-${minecraftVersion}"
 }
 
 dependencies {
 	minecraft("com.mojang:minecraft:${minecraftVersion}")
-	mappings("net.fabricmc:yarn:${properties["mappings_version"].toString()}:v2")
-	modImplementation("net.fabricmc:fabric-loader:${properties["fabric_loader_version"].toString()}")
-	modImplementation("net.fabricmc.fabric-api:fabric-api:${properties["fabric_api_version"].toString()}")
+	mappings("net.fabricmc:yarn:${properties["mappings_version"]}:v2")
+	modImplementation("net.fabricmc:fabric-loader:${properties["fabric_loader_version"]}")
+	modImplementation("net.fabricmc.fabric-api:fabric-api:${properties["fabric_api_version"]}")
 }
 
 loom {
+	runs {
+		named("client") {
+			runDir = "run/client"
+		}
+		named("server") {
+			runDir = "run/server"
+		}
+	}
+
 	mods {
-		create(properties["mod_id"].toString()) {
+		create(modId) {
 			sourceSet("main")
 		}
 	}
@@ -26,7 +36,12 @@ loom {
 
 java {
 	toolchain.languageVersion = JavaLanguageVersion.of(8)
+
 	withSourcesJar()
+}
+
+tasks.withType(JavaCompile::class.java).configureEach {
+	options.encoding = "UTF-8"
 }
 
 publishing {
@@ -50,9 +65,6 @@ publishing {
 				password = System.getenv("GITHUB_TOKEN")
 			}
 		}
+		mavenLocal()
 	}
-}
-
-tasks.withType(JavaCompile::class.java).configureEach {
-	options.encoding = "UTF-8"
 }
